1a6cd2fff7a27959aaedfa952f25b0878f680758
AlexHeilong <alexdeev@mail.ru>
2022-07-12 01:00:03 +0800

    add second commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int num;
+
+Console.Write("input integer number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your number is " + num);
+
---
+++ Семинар 1/Lesson_1/Program.cs
@@ -0,0 +1,6 @@
+﻿int num;
+
+Console.Write("input integer number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your number is " + num);
